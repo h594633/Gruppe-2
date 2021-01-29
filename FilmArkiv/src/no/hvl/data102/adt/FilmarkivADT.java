f098fa3a3f767bba6fd859701df4282461f87dae
h595488 <jorgenvikan@gmail.com>
2021-01-29 13:11:38 +0100

    adt som gitt i canvas

--- FilmArkiv/src/no/hvl/data102/adt/FilmarkivADT.java
+++ FilmArkiv/src/no/hvl/data102/adt/FilmarkivADT.java
@@ -1,8 +1,22 @@
 package no.hvl.data102.adt;
+
 import no.hvl.dat102.Film;
+import no.hvl.dat102.Sjanger;
+
 public interface FilmarkivADT {
+	
+	Film[] hentFilmTabell();
 	
 	void LeggTilFilm(Film nyFilm);
 	
+	boolean slettFilm(int filmnr);
 	
+	Film[] soekTittel(String delstreng);
+	
+	Film[] soekProdusent(String delstreng);
+	
+	int antall (Sjanger sjanger);
+	
+
+    int antall();
 }
